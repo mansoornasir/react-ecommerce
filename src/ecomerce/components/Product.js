ab3cdeab3e1ecf3d214f06fb91f6a86f495c081e
mansoornasir <mrmansoornasir@gmail.com>
2022-09-07 16:29:52 +0500

    removed product page

--- src/ecomerce/components/Product.js
+++ src/ecomerce/components/Product.js
@@ -1,37 +1,9 @@
-import React, {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import "react-notifications/lib/notifications.css";
-import {NotificationManager} from "react-notifications"
-
+import React from 'react'
 const Product = () => {
-  const params = useParams();
-  const [product, setProduct] = useState({});
-  useEffect(() => {
-    const token = localStorage.getItem("token")
-    if(!token) return NotificationManager.error("You need to login first.","",3000)
-    axios.get(
-      "https://api-try-n-save.herokuapp.com/api/product?pid="+params.id, {
-      headers: {
-        token: token
-      }
-    }).then(res => {
-          setProduct(res.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-  }, [])
 
   return (
     <div>
-      <p>{product.id}</p>
-      <p>{product.title}</p>
-      <p>{product.description}</p>
-      <p>{product.category}</p>
-      <p>{product.rating.rate}</p>
-      <p>{product.price}</p>
-      <p><img src={product.image} width={300} height={300}/></p>
+      <h2>We are at product page.</h2>
     </div>
   )
 }
